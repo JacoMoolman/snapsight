d1a5afa78204cbbdd81bef266c44843ad80050c8
Jaco Moolman <88827378+JacoMoolman@users.noreply.github.com>
2024-02-24 16:35:40 +0200

    Update llavainf.py

--- llavainf.py
+++ llavainf.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 def get_description(filename):
-    command = ["ollama", "run", "llava:13b", "What is in this image", filename]
+    command = ["ollama", "run", "llava:13b", "I am blind, please describe this image in as much detail as possible so I know what is going on. Only give me feedback on the image, no other issues or anything else. Describe the image in as much detail as possible!", filename]
     output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     command_output = output.stdout
     return command_output
